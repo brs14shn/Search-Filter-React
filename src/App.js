4bfe2aaadde14446ed6eda5b0ea639198c17d9e1
brs14shn <brsshn292929@gmail.com>
2022-08-11 02:01:13 +0300

    filtered

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
+import { useState} from 'react';
 import './App.css';
+import {data} from "./helper/data"
 
 function App() {
+  const [search, setSearch] = useState([])
+
+  const handleFilter=(e)=>{
+    const newWord=e.target.value
+    console.log(newWord);
+    const filter=data.filter((value)=>{
+      return value.first_name.toLowerCase().includes(newWord.toLowerCase)
+    })
+    setSearch(filter)
+  }
+
+
+
+
+  
   return (
     <div className="App">
+      <input type="text" placeholder='Search...' onChange={handleFilter} />
+      
+        {search.map((val,key)=>{
+          return(
+            <div>
+              <p>{val.first_name}</p>
+            </div>
+          )
+        })
+      }
       
     </div>
   );
